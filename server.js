dc4b59a5092591c488c0ef8e34f7ec5c7bf8910d
DanielJandric <daniel.jandric@becapital.ch>
2025-06-05 18:09:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const OpenAI = require('openai');
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const app = express();
 const port = process.env.PORT || 3000;
